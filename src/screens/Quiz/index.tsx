3ca3e4d2506823bdae953112a4d2cd6eea1b11a9
Daniel Chrisostomo <daniluz1515@gmail.com>
2024-07-02 12:04:10 +0200

    Animando o OverlayFeedback

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -18,6 +18,7 @@ import { ConfirmButton } from '../../components/ConfirmButton';
 import { OutlineButton } from '../../components/OutlineButton';
 import { ProgressBar } from '../../components/ProgressBar';
 import { SimultaneousGesture } from 'react-native-gesture-handler/lib/typescript/handlers/gestures/gestureComposition';
+import { OverLayFeedback } from '../../components/OverLayFeedback';
 
 interface Params {
   id: string;
@@ -34,6 +35,8 @@ export function Quiz() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [quiz, setQuiz] = useState<QuizProps>({} as QuizProps);
   const [alternativeSelected, setAlternativeSelected] = useState<null | number>(null);
+  const [statusReply, setStatusReply] = useState(0)
+
 
   const shake = useSharedValue(0)
   const scrollY = useSharedValue(0)
@@ -80,8 +83,10 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
+      setStatusReply(1)
       setPoints(prevState => prevState + 1);
     } else {
+      setStatusReply(2)
       shakeAnimation()
     }
 
@@ -185,7 +190,7 @@ export function Quiz() {
 
   return (
     <View style={styles.container}>
-
+    <OverLayFeedback status={statusReply} />
     <Animated.View style={fixedProgressBarStyles}>
       <Text style={styles.title}>{quiz.title}</Text>
 
